8253519820cb5e6235390ac3f6396a5b8eb6e30a
Mikhail Malygin <mmalygin@naumen.ru>
2025-05-27 19:37:14 +0500

    added hellowWorldTest

--- src/test/java/HelloWorldTests.java
+++ src/test/java/HelloWorldTests.java
@@ -1,9 +1,22 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTests {
 
     @Test
     public void helloWorldTest() {
-        System.out.println("Hello World!");
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Mikhail");
+
+        Response response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
